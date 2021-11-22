fcbab747efcf1ecd38136a6418b093ccdb3fcbca
Sofie C <sofie.claridge@gmail.com>
2021-11-23 10:59:25 +1300

    init & formating

--- trunk/SUAVE/Analyses/Mission/Segments/Conditions/Basic.py
+++ trunk/SUAVE/Analyses/Mission/Segments/Conditions/Basic.py
@@ -79,7 +79,7 @@ class Basic(Conditions):
         # weights conditions
         self.weights.total_mass            = ones_1col * 0
         self.weights.weight_breakdown      = Conditions()
-        self.weights.extra_mass      = ones_1col * 0
+        self.weights.extra_mass            = ones_1col * 0
         self.weights.fuel_mass             = ones_1col * 0
         
         # energy conditions
--- trunk/SUAVE/Components/Energy/Cooling/__init__.py
+++ trunk/SUAVE/Components/Energy/Cooling/__init__.py
@@ -7,4 +7,4 @@
 # Created:  Feb 2020, K.Hamilton
 
 from .Cryocooler import Cryocooler
-
+from .Cryogenic_Heat_Exchanger import Cryogenic_Heat_Exchanger
